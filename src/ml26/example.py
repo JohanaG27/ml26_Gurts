7184b824045370fdc264420ea99f8ed509d056ef
johana <johana.guardado@cetys.edu.mx>
2026-02-25 19:28:09 -0800

    johana 2

--- src/ml26/example.py
+++ src/ml26/example.py
@@ -1,14 +1,7 @@
 def fcn(a, b):
-<<<<<<< HEAD
     total = 100 + pow(a, b)
     return total
 
 
 print(fcn(10, 2))
 # Hola, companeros!
-=======
-    return a + b
-
-# hola me llamo jose jejejejeksjkwjkjs
-# Este es el comentario de JpAboytes
->>>>>>> upstream/master
